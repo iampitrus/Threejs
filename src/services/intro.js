70a0665726cd3931b7d98adf954ca70acd10ec77
Ogbonnaya Peter <ogbonnapete210@gmail.com>
2024-11-24 23:07:44 +0100

    handle resize, object appear good even upon resizing the browser

--- src/services/intro.js
+++ src/services/intro.js
@@ -16,23 +16,23 @@ export function renderCube() {
   // The Camera - Determines what is viewed
   // inside camera - const camera = new THREE.PerspectiveCamera(fov,aspect-ratio,near,far)
   // fov -field of view, it's just like a zoom, it's measured in degree, the smaller the value, the more the zoom
-  //   const camera = new THREE.PerspectiveCamera(
-  //     50,
-  //     window.innerWidth / window.innerHeight,
-  //     0.1,
-  //     30
-  //   );
+  const camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    30
+  );
 
   const aspectRatio = window.innerWidth / window.innerHeight;
 
-  const camera = new THREE.OrthographicCamera(
-    -1 * aspectRatio,
-    1 * aspectRatio,
-    1,
-    -1,
-    0.1,
-    200
-  );
+  //   const camera = new THREE.OrthographicCamera(
+  //     -1 * aspectRatio,
+  //     1 * aspectRatio,
+  //     1,
+  //     -1,
+  //     0.1,
+  //     200
+  //   );
 
   // shift the camera back on the z axis
   camera.position.z = 5;
@@ -41,12 +41,19 @@ export function renderCube() {
   const canvas = document.querySelector("canvas.threejs");
   const renderer = new THREE.WebGLRenderer({ canvas });
 
+  // we have orbitControl, pointerLockControl, trackballControls, dragControls
+
   // Initialize the orbit controls
   const controls = new OrbitControls(camera, canvas);
   controls.autoRotate = true;
   controls.enableDamping = true;
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
 
   // incharge of triggering a render when we have a new image
   // window.requestAnimationFrame(renderloop);
